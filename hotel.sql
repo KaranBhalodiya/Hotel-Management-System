2491f724a87b19c453ea094ba71ae7348312700f
TheOperatic <56972654+KaranBhalodiya@users.noreply.github.com>
2021-10-21 17:05:36 +0530

    Add files via upload

--- hotel.sql
+++ hotel.sql
@@ -1,20 +1,21 @@
 -- phpMyAdmin SQL Dump
--- version 4.2.7.1
--- http://www.phpmyadmin.net
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 14, 2017 at 06:52 PM
--- Server version: 5.6.20
--- PHP Version: 5.5.15
+-- Generation Time: Oct 21, 2021 at 01:34 PM
+-- Server version: 10.4.13-MariaDB
+-- PHP Version: 7.4.7
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `hotel`
@@ -23,50 +24,21 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `contact`
---
-
-CREATE TABLE IF NOT EXISTS `contact` (
-`id` int(10) unsigned NOT NULL,
-  `fullname` varchar(100) DEFAULT NULL,
-  `phoneno` int(10) DEFAULT NULL,
-  `email` text,
-  `cdate` date DEFAULT NULL,
-  `approval` varchar(12) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `login`
 --
 
-CREATE TABLE IF NOT EXISTS `login` (
-`id` int(10) unsigned NOT NULL,
+CREATE TABLE `login` (
+  `id` int(10) UNSIGNED NOT NULL,
   `usname` varchar(30) DEFAULT NULL,
   `pass` varchar(30) DEFAULT NULL
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `login`
 --
 
 INSERT INTO `login` (`id`, `usname`, `pass`) VALUES
-(1, 'Admin', '1234'),
-(2, 'Prasath', '12345');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `newsletterlog`
---
-
-CREATE TABLE IF NOT EXISTS `newsletterlog` (
-`id` int(10) unsigned NOT NULL,
-  `title` varchar(52) DEFAULT NULL,
-  `subject` varchar(100) DEFAULT NULL,
-  `news` text
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+(1, 'Admin', '1234');
 
 -- --------------------------------------------------------
 
@@ -74,7 +46,7 @@ CREATE TABLE IF NOT EXISTS `newsletterlog` (
 -- Table structure for table `payment`
 --
 
-CREATE TABLE IF NOT EXISTS `payment` (
+CREATE TABLE `payment` (
   `id` int(11) DEFAULT NULL,
   `title` varchar(5) DEFAULT NULL,
   `fname` varchar(30) DEFAULT NULL,
@@ -89,8 +61,17 @@ CREATE TABLE IF NOT EXISTS `payment` (
   `mepr` double(8,2) DEFAULT NULL,
   `meal` varchar(30) DEFAULT NULL,
   `btot` double(8,2) DEFAULT NULL,
-  `noofdays` int(11) DEFAULT NULL
+  `noofdays` int(11) DEFAULT NULL,
+  `croom` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `payment`
+--
+
+INSERT INTO `payment` (`id`, `title`, `fname`, `lname`, `troom`, `tbed`, `nroom`, `cin`, `cout`, `ttot`, `fintot`, `mepr`, `meal`, `btot`, `noofdays`, `croom`) VALUES
+(8, 'Dr.', 'Karan', 'Bhalodiya', 'Superior Room', 'Double', 1, '2021-10-16', '2021-10-17', 320.00, 345.60, 19.20, 'Half Board', 6.40, 1, 'Jaipur'),
+(10, 'Mr.', 'Jay', 'Patel', 'Superior Room', 'Quad', 2, '2021-10-21', '2021-10-23', 1280.00, 1408.00, 102.40, 'Full Board', 25.60, 2, 'Jodhpur');
 
 -- --------------------------------------------------------
 
@@ -98,26 +79,26 @@ CREATE TABLE IF NOT EXISTS `payment` (
 -- Table structure for table `room`
 --
 
-CREATE TABLE IF NOT EXISTS `room` (
-`id` int(10) unsigned NOT NULL,
+CREATE TABLE `room` (
+  `id` int(10) UNSIGNED NOT NULL,
   `type` varchar(15) DEFAULT NULL,
   `bedding` varchar(10) DEFAULT NULL,
   `place` varchar(10) DEFAULT NULL,
   `cusid` int(11) DEFAULT NULL
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=16 ;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `room`
 --
 
 INSERT INTO `room` (`id`, `type`, `bedding`, `place`, `cusid`) VALUES
-(1, 'Superior Room', 'Single', 'Free', NULL),
-(2, 'Superior Room', 'Double', 'Free', NULL),
+(1, 'Superior Room', 'Single', 'NotFree', 3),
+(2, 'Superior Room', 'Double', 'Free', 0),
 (3, 'Superior Room', 'Triple', 'Free', NULL),
 (4, 'Single Room', 'Quad', 'Free', NULL),
-(5, 'Superior Room', 'Quad', 'Free', NULL),
+(5, 'Superior Room', 'Quad', 'NotFree', 10),
 (6, 'Deluxe Room', 'Single', 'Free', NULL),
-(7, 'Deluxe Room', 'Double', 'Free', NULL),
+(7, 'Deluxe Room', 'Double', 'Free', 0),
 (8, 'Deluxe Room', 'Triple', 'Free', NULL),
 (9, 'Deluxe Room', 'Quad', 'Free', NULL),
 (10, 'Guest House', 'Single', 'Free', NULL),
@@ -133,15 +114,15 @@ INSERT INTO `room` (`id`, `type`, `bedding`, `place`, `cusid`) VALUES
 -- Table structure for table `roombook`
 --
 
-CREATE TABLE IF NOT EXISTS `roombook` (
-`id` int(10) unsigned NOT NULL,
+CREATE TABLE `roombook` (
+  `id` int(10) UNSIGNED NOT NULL,
   `Title` varchar(5) DEFAULT NULL,
-  `FName` text,
-  `LName` text,
+  `FName` text DEFAULT NULL,
+  `LName` text DEFAULT NULL,
   `Email` varchar(50) DEFAULT NULL,
   `National` varchar(30) DEFAULT NULL,
   `Country` varchar(30) DEFAULT NULL,
-  `Phone` text,
+  `Phone` text DEFAULT NULL,
   `TRoom` varchar(20) DEFAULT NULL,
   `Bed` varchar(10) DEFAULT NULL,
   `NRoom` varchar(2) DEFAULT NULL,
@@ -149,72 +130,63 @@ CREATE TABLE IF NOT EXISTS `roombook` (
   `cin` date DEFAULT NULL,
   `cout` date DEFAULT NULL,
   `stat` varchar(15) DEFAULT NULL,
-  `nodays` int(11) DEFAULT NULL
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+  `nodays` int(11) DEFAULT NULL,
+  `CRoom` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `roombook`
+--
+
+INSERT INTO `roombook` (`id`, `Title`, `FName`, `LName`, `Email`, `National`, `Country`, `Phone`, `TRoom`, `Bed`, `NRoom`, `Meal`, `cin`, `cout`, `stat`, `nodays`, `CRoom`) VALUES
+(10, 'Mr.', 'Jay', 'Patel', 'jp@gmail.com', 'Indian', 'India', '123', 'Superior Room', 'Quad', '2', 'Full Board', '2021-10-21', '2021-10-23', 'Conform', 2, 'Jodhpur'),
+(11, 'Mr.', 'kaushik', 'ruparel', 'kr@gmail.com', 'Indian', 'Guinea-Bissau', '123', 'Superior Room', 'Double', '1', 'Breakfast', '2021-10-21', '2021-10-20', 'Not Conform', -1, 'Jodhpur');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `contact`
---
-ALTER TABLE `contact`
- ADD PRIMARY KEY (`id`);
-
---
 -- Indexes for table `login`
 --
 ALTER TABLE `login`
- ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `newsletterlog`
---
-ALTER TABLE `newsletterlog`
- ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `room`
 --
 ALTER TABLE `room`
- ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `roombook`
 --
 ALTER TABLE `roombook`
- ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT for table `contact`
---
-ALTER TABLE `contact`
-MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT;
---
 -- AUTO_INCREMENT for table `login`
 --
 ALTER TABLE `login`
-MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
---
--- AUTO_INCREMENT for table `newsletterlog`
---
-ALTER TABLE `newsletterlog`
-MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
 --
 -- AUTO_INCREMENT for table `room`
 --
 ALTER TABLE `room`
-MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=16;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+
 --
 -- AUTO_INCREMENT for table `roombook`
 --
 ALTER TABLE `roombook`
-MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
